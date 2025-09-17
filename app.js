541c55ac3b1ebd30f63ffb3e66b26dc3d9ae91f2
ok1naldo <okenov.ema@gmail.com>
2025-09-17 21:18:25 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+// Плавный скролл
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
